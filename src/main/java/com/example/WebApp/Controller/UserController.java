dac843d7c4fcb5b3953d0e3652473d8f378f53f8
EB20091520 <EB20091520@192.168.0.30>
2026-01-30 11:23:43 +0900

    lishiyu0417

--- src/main/java/com/example/WebApp/Controller/UserController.java
+++ src/main/java/com/example/WebApp/Controller/UserController.java
@@ -3,7 +3,11 @@ package com.example.WebApp.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.WebApp.Entity.User;
 import com.example.WebApp.Repository.UserRepository;
 
 @Controller
@@ -20,4 +24,30 @@ public class UserController {
         model.addAttribute("users", userRepository.findAll());
         return "users";
     }
+    
+    @GetMapping("/addUser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user";
+    }
+    
+    @GetMapping("/editUser/{id}")
+    public String editUser(Model model, @PathVariable Long id) {
+    	User user = userRepository.findById(id).orElse(null);
+        model.addAttribute("user", user);
+        return "user";
+    }
+    
+    @PostMapping("/saveUser")
+    public String save(@ModelAttribute User user) {
+    	userRepository.save(user);
+    	return "redirect:/users";
+    }
+    
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Long id) {
+    	userRepository.deleteById(id);
+    	return "redirect:/users";
+    }
+    
 }
